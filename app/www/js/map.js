3eb10cb14779a515802acb9105dcefaad68f7911
David <david.farrow@griffithuni.edu.au>
2016-07-31 10:51:44 +1000

    Map styling

--- app/www/js/map.js
+++ app/www/js/map.js
@@ -37,11 +37,16 @@ var map = (function (App) {
             var lat = position.coords["latitude"];
             var lng = position.coords["longitude"];
 
+
+            var styles = [{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#e9e5dc"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"poi.medical","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"poi.sports_complex","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54},{"visibility":"off"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"water","elementType":"all","stylers":[{"saturation":43},{"lightness":-11},{"color":"#89cada"}]}];
+
             mapview = new google.maps.Map(document.getElementById('map'), {
                 center: {lat: lat, lng: lng},
                 scrollwheel: false,
                 zoom: 16
             });
+
+            mapview.setOptions({styles: styles});
 
             marker = new google.maps.Marker({
                 map: mapview,
