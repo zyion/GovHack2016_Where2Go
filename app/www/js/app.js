c410ca2bacbb78bb8c2dc198b9018521f9f62428
zyion <zyionattiig@gmail.com>
2016-07-31 13:30:24 +1000

    Updates to app.js

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -21,6 +21,66 @@ var App = (function() {
         }
     };
 
+    var loadCsv = function (url, callback) {
+        $.ajax({
+            method: 'GET',
+            url: url,
+            datatype: 'text'
+        }).done(function(html) {
+            callback(html);
+        });
+    };
+
+    var parseCsv = function (csv, callback) {
+        var lines = csv.split('\n');
+        var headers = lines[0].split(',');
+        var records = [];
+        for (var i = 1; i < lines.length; i++) {
+            var record = {};
+            if (lines[i] !== "") {
+                var row = lines[i].split(',');
+                if (row.length == headers.length) {
+                    for (var j = 0; j < headers.length; j++) {
+                        record[headers[j].trim()] = row[j].trim();
+                    }
+                    records.push(record);
+                }
+            }
+        }
+        callback(records);
+    };
+
+    var parseFuckedCsv = function (csv, callback) {
+        var lines = csv.split('\n');
+        var headers = lines[0].split(',');
+        var records = [];
+        for (var i = 1; i < lines.length; i++) {
+            var record = {};
+            if (lines[i] !== "") {
+                var row = lines[i].split(',');
+                if (row.length == headers.length) {
+                    for (var j = 0; j < headers.length; j++) {
+                        record[headers[j].trim()] = row[j].trim();
+                    }
+                    records.push(record);
+                } else {
+                     for (var j = 0; j < headers.length; j++) {
+                        if (row[j].trim().startsWith('"')) {
+                            var value = row[j].trim();
+                            var index = 0;
+                            while (!row[j + index].trim().endsWith('"')) {
+                                value.concat(row[j + index].trim());
+                            }
+                        }
+
+                        record[headers[j].trim()] = row[j].trim();
+                    }
+                }
+            }
+        }
+        callback(records);
+    };
+
     var layers = [];
 
     var loadDataLayer = function(name) {
@@ -30,20 +90,92 @@ var App = (function() {
         var list = dal.getListOf(name);
         for (var i in list) {
             if (i > 5) break; // Google maps limit of 10 data layers
-            layers.push(map.geoData(list[i]['url']));
+            layers.push(map.geoData(list[i]));
         }
     };
 
     var loadMenu = function() {
         loadTemplate('./views/toggle_list.html', function(template) {
             if (template) {
-                $('#toggle-list').html(populateTemplate(template, {data: dal.getCatergories}));
+                $('#toggle-list').append(populateTemplate(template, {data: dal.getCatergories}));
             } else {
                 console.log('toggle_list.html not found!');
             }
         });
         $(document).on('click', '.toggle-item', function (event) {
             loadDataLayer(event.target.id);
+        });
+
+        // Transport
+        $(document).on('click', '#transport', function (event) {
+            
+            loadCsv('./data/SEQ_GTFS/stops.txt', function(csv) {
+                parseCsv(csv, function (records) {
+                    console.log(records);
+                    for (var i in records) {
+                        var record = records[i];
+                        map.addMarker({
+                            position: {
+                                lat: parseInt(record['stop_lat']),
+                                lng: parseInt(record['stop_lng'])
+                            },
+                            title: record['stop_name'],
+                            label: 'Stop'
+                        });
+                    }
+
+                });
+            });
+            
+        });
+
+        // Comm Games Venues
+        $(document).on('click', '#commonwealthGames', function (event) {
+            
+            loadCsv('./data/gold-coast-2018-commonwealth-games-competition-venues.csv', function(csv) {
+                parseCsv(csv, function (records) {
+
+                    console.log(records);
+
+                    for (var i in records) {
+                        var record = records[i];
+                        var options = {
+                            position: {
+                                lat: parseInt(record['Latitude']),
+                                lng: parseInt(record['Longitude'])
+                            },
+                            title: record['Venue'],
+                            label: record['Sport']
+                        };
+                        console.log(record);
+                        console.log(options);
+                        map.addMarker(options);
+                    }
+
+                });
+            });
+            
+        });
+
+        // Beaches
+        $(document).on('click', '#beaches', function (event) {
+            
+            $.ajax({
+                url: 'https://data.qld.gov.au/api/action/datastore_search',
+                datatype: 'jsonp'
+            }).done(function(data) {
+                console.log(data)
+            });
+
+            //loadCsv('https://data.qld.gov.au/dataset/e9a4670f-4d1f-4c56-9855-dbcb103cebfd/resource/2bbef99e-9974-49b9-a316-57402b00609c/view/db26be1e-a115-497b-85b6-e5970dac7c2d', function(csv) {
+            loadCsv('https://data.qld.gov.au/dataset/coastal-data-system-near-real-time-wave-data/resource/2bbef99e-9974-49b9-a316-57402b00609c', function(csv) {
+                parseCsv(csv, function (records) {
+
+                    //console.log(records);
+
+                });
+            });
+            
         });
     };
 
@@ -72,7 +204,10 @@ var App = (function() {
 
     var loaded = function() {
 
+
+
     };
+
 
     return {
         displayPage: displayPage,
